b8b63b9069be0053375c1642ab5d9a13e5d7e0f0
Ruben Ramirez <rramirez110911@gmail.com>
2023-02-24 23:25:02 -0800

    imports home components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Hero from '../Components/Home/Hero'
+import Benefits from '../Components/Home/Benefits'
+import Services from '../Components/Home/Services'
+import About from '../Components/Home/About'
+import Testimonials from '../Components/Home/Testimonials'
 
 function Home() {
-  return <div>This is Home</div>
+  return (
+    <main>
+      <Hero />
+      <Benefits />
+      <Services />
+      <About />
+      <Testimonials />
+    </main>
+  )
 }
 
 export default Home
