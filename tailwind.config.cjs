ae44baec5eb834f040b981139f83da2531bedd6c
Ruben Ramirez <rramirez110911@gmail.com>
2023-02-24 23:20:46 -0800

    changes color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'dark-blue': '#023047',
+      'light-orange': '#ffb703',
+      'dark-orange': '#fb8500',
+      'off-white': '#F7F6F2',
+      white: '#FDFDFC',
+    },
     extend: {},
   },
   plugins: [],
