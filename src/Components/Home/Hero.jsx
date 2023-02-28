8254d6e02a45ba0c2b17073515eb9733baecd8fa
Ruben Ramirez <rramirez110911@gmail.com>
2023-02-27 22:33:20 -0800

    adds background image and clips path

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
 function Hero() {
-  return <div>Hero</div>
+  return (
+    <section className='w-full h-full bg-hero-pattern bg-fixed bg-cover pentagon'>
+      <div className='w-full h-full flex flex-row items-center pl-24'>
+        <div className='w-1/3 flex flex-col items-start gap-12 -translate-y-12'>
+          <h1 className='text-5xl '>
+            <span className='text-light-orange'> BUILDING STRONG ROOFS</span>{' '}
+            FOR STRONG HOMES
+          </h1>
+          <h2 className='text-lg'>
+            So Cal's top-quality roofing service provider delivers long-term
+            value and ensures the safety and protection of homes and businesses.
+          </h2>
+          <button className='btn'>Get a Quote</button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
