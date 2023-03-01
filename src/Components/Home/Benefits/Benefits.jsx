3ded1e192c6a0497c1ff921c0211d2bec9052a9e
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-01 10:51:43 -0800

    creates section element and and imports benefit component

---
+++ src/Components/Home/Benefits/Benefits.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Benefit from './Benefit'
+import { Clock, AlertTriangle, PiggyBank } from 'lucide-react'
+
+function Benefits() {
+  return (
+    <section className='flex flex-col items-center text-dark-blue gap-24'>
+      <h2 className=' text-4xl font-bold'>
+        THE BENEFITS OF HIRING PREMIER ROOFING CO.
+      </h2>
+      <div className='flex flex-row justify-center gap-16'>
+        <Benefit
+          children={<Clock fill='#ffb703' size={64} />}
+          heading={'TIME-SAVING'}
+          text={
+            'We have the resources to complete the job quickly and efficiently, saving you time and effort.'
+          }
+        />
+        <Benefit
+          children={<AlertTriangle fill='#ffb703' size={64} />}
+          heading={'SAFETY'}
+          text={
+            'The job is done safely, minimizing the risk of accidents and injuries.'
+          }
+        />
+        <Benefit
+          children={<PiggyBank fill='#ffb703' size={64} />}
+          heading={'COST-EFFECTIVE'}
+          text={
+            'Minimizes the need for future repairs by doing the job right the first time around.'
+          }
+        />
+      </div>
+      <button className='btn'>Get a Quote</button>
+    </section>
+  )
+}
+
+export default Benefits
