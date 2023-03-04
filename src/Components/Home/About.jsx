da06bc85158a60f8b8e3870a2188ddcc066c81c1
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-03 21:33:29 -0800

    creates layout

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
 function About() {
-  return <div>About</div>
+  return (
+    <section>
+      <div>
+        <h2>WHO WE ARE</h2>
+        <p>
+          Premier Roofing Co. is dedicated to providing exceptional roofing
+          solutions that protect and enhance the value of our customers' homes
+          and businesses. Our mission is to deliver the highest quality roofing
+          services with integrity, professionalism, and reliability. We strive
+          to exceed our customers' expectations by providing personalized
+          attention, expert craftsmanship, and cost-effective solutions that
+          meet their unique needs. We are committed to promoting safety,
+          sustainability, and innovation in everything we do. At Premier Roofing
+          Co., our goal is to build lasting relationships with our customers by
+          consistently delivering exceptional service and exceptional results.
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default About
