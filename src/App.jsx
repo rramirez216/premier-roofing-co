ab7e73f56502645128cab55211d5e9a3d1279f34
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-07 22:16:22 -0800

    removes grid and main grid utility classes from the outermost div

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import { Routes, Route, Outlet, Link } from 'react-router-dom'
 import Home from './Pages/Home'
 import Nav from './Components/Nav'
-import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
-    <div className='h-full grid main-grid text-white text-lg'>
+    <div className='h-full text-white text-lg'>
       <Nav />
       <Routes>
         <Route index element={<Home />} />
       </Routes>
-      <Footer />
     </div>
   )
 }
