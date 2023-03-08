ea97efea74ddefa839a71d80adcc063b44cda3f4
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-07 22:18:14 -0800

    imports and renders footer component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Hero from '../Components/Home/Hero'
 import Benefits from '../Components/Home/Benefits/Benefits'
-import Services from '../Components/Home/Services'
+import Services from '../Components/Home/Services/Services'
 import About from '../Components/Home/About'
 import Testimonials from '../Components/Home/Testimonials'
+import Footer from '../Components/Footer/Footer'
 
 function Home() {
   return (
-    <main>
+    <main className='h-full'>
       <Hero />
       <Benefits />
       <Services />
       <About />
       <Testimonials />
+      <Footer />
     </main>
   )
 }
