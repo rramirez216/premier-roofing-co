7a4fa29b767d74a5ab7a80ce7b91a2963344e5e2
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-09 20:07:45 -0800

    imports swiper components and creates a figure element that holds the quote for the testimonials

--- src/Components/Home/Testimonials.jsx
+++ src/Components/Home/Testimonials.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import testimonialsArray from '../../data/testimonials'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Autoplay, Pagination } from 'swiper'
 
 function Testimonials() {
   return (
-    <div>
-      <div>this is where the Testimonials will go</div>
+    <div className='pt-20 pb-20 bg-dark-blue flex justify-center'>
+      <div className='border-t border-b border-dark-orange pt-8 pb-8 w-1/2'>
+        <Swiper
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Autoplay, Pagination]}
+        >
+          {testimonialsArray.map((value) => (
+            <SwiperSlide key={value.id} className='flex justify-center'>
+              <figure className='max-w-lg text-center'>
+                <blockquote className='text-2xl pb-4'>{value.quote}</blockquote>
+                <figcaption className='text-center'>- {value.name}</figcaption>
+              </figure>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   )
 }
