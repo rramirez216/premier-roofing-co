97efdf12daa248c40ecd90af89a4a277c5685a5c
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-13 14:19:14 -0700

    creates testimonials array to display in the testimonials section of app

---
+++ src/data/testimonials.js
@@ -0,0 +1,34 @@
+const testimonialsArray = [
+  {
+    id: 0,
+    name: 'Samuel Davis',
+    quote:
+      '"I highly recommend Premier Roofing Co. for their outstanding workmanship and professionalism. They completed our roof replacement in a timely manner and went above and beyond to ensure our satisfaction."',
+  },
+  {
+    id: 1,
+    name: 'Rebecca Hall',
+    quote:
+      '"Premier Roofing Co. is the best roofing service provider in town! They provided us with top-quality materials and excellent customer service. We are extremely pleased with the final results!"',
+  },
+  {
+    id: 2,
+    name: 'Christopher Jones',
+    quote:
+      '"The team at Premier Roofing Co. is amazing! They were very helpful and informative throughout the entire process. Our new roof looks amazing and we couldn\'t be happier with the service we received."',
+  },
+  {
+    id: 3,
+    name: 'Isabella Wilson',
+    quote:
+      '"I had a great experience working with Premier Roofing Co. They were very responsive, provided me with a detailed estimate, and completed the job on time and within budget. I would definitely recommend them to anyone in need of roofing services."',
+  },
+  {
+    id: 4,
+    name: 'Robert Garcia',
+    quote:
+      '"Premier Roofing Co. is a top-notch company with exceptional customer service. They were very patient with all of my questions and concerns and delivered a beautiful new roof for my home. I would highly recommend them to anyone!"',
+  },
+]
+
+export default testimonialsArray
