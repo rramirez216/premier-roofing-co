09f42f458d262b50a1ffc1b3a34cc414dcf69d11
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-13 14:12:37 -0700

    removes extra white space on line 9

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -6,7 +6,7 @@ function Nav() {
   return (
     <nav className='flex justify-center fixed top-0 left-0 right-0 z-10'>
       <div className='w-3/4 flex items-center gap-8'>
-        <Link to='/' className=' w-32 h-32 flex'>
+        <Link to='/' className='w-32 h-32'>
           <img src={Logo} alt='logo' className='' />
         </Link>
         <div className='border-t border-b-0 border-white flex-auto'></div>
