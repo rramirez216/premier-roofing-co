d227f7af80aec6c24fc58b6b6813e8c54874ad99
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-13 14:20:00 -0700

    creates footer content such as addresss location and adds logo

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import { useLoadScript } from '@react-google-maps/api'
+import Map from './Map'
+import ContactForm from './ContactForm'
+import Logo from '../../assets/Logo.png'
 
 function Footer() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: '',
+  })
+  if (!isLoaded) return <div>Loading...</div>
   return (
-    <div id='contact' className='text-dark-blue'>
-      TThis is a footter
-    </div>
+    <footer className='pb-4 bg-dark-blue'>
+      <Map />
+      <div className='flex flex-col gap-12 text-sm pt-20'>
+        <div className='flex'>
+          <div className='flex-1 flex flex-row justify-center gap-10'>
+            <div className='w-40'>
+              <h3 className='text-lg mb-4 font-semibold'>Address</h3>
+              <p className=''>2896 Metropolitan Pl Pomona, CA 91767</p>
+            </div>
+            <div className='w-40'>
+              <h3 className='text-lg mb-4 font-semibold'>Contact</h3>
+              <p>info@premierroofingco.com</p>
+              <p>(909) 232-4598</p>
+            </div>
+            <div className='w-40'>
+              <h3 className='text-lg mb-4 font-semibold'>Hours</h3>
+              <p>Mon - Fri: 8 am - 7 pm</p>
+            </div>
+          </div>
+          <ContactForm />
+        </div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='w-24 h-24'>
+            <img src={Logo} alt='logo' className='' />
+          </div>
+          <p> &copy; 2023 Premier Roofing Co. All Rights Reserved</p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
