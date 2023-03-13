9e0983e795e6b4777b19299cb30fe46ed4ffdaa3
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-13 14:17:10 -0700

    removes unused swiper modules

--- src/Components/Home/Testimonials.jsx
+++ src/Components/Home/Testimonials.jsx
@@ -3,22 +3,20 @@ import testimonialsArray from '../../data/testimonials'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
-import { Autoplay, Pagination } from 'swiper'
+import { Autoplay } from 'swiper'
 
 function Testimonials() {
   return (
-    <div className='pt-20 pb-20 bg-dark-blue flex justify-center'>
+    <section className='pt-20 pb-20 bg-dark-blue flex justify-center'>
       <div className='border-t border-b border-dark-orange pt-8 pb-8 w-1/2'>
         <Swiper
           centeredSlides={true}
+          loop={true}
           autoplay={{
-            delay: 2500,
+            delay: 5500,
             disableOnInteraction: false,
           }}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Autoplay, Pagination]}
+          modules={[Autoplay]}
         >
           {testimonialsArray.map((value) => (
             <SwiperSlide key={value.id} className='flex justify-center'>
@@ -30,7 +28,7 @@ function Testimonials() {
           ))}
         </Swiper>
       </div>
-    </div>
+    </section>
   )
 }
 
