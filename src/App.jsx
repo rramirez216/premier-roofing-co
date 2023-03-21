9791f5fa17868dd58857e52d580ce6590ba43ede
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-21 14:43:08 -0700

    imports contact form and servicespage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-import { Routes, Route, Outlet, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import Home from './Pages/Home'
 import Nav from './Components/Nav'
+import ContactForm from './Components/Footer/ContactForm'
+import ServicesPage from './Pages/ServicesPage'
 
 function App() {
+  const [toggleContact, setToggleContact] = useState(false)
   return (
-    <div className='h-full text-white text-lg'>
+    <div className='h-full text-white text-lg relative'>
       <Nav />
       <Routes>
-        <Route index element={<Home />} />
+        <Route index element={<Home setToggleContact={setToggleContact} />} />
+        <Route
+          path='/services'
+          element={<ServicesPage setToggleContact={setToggleContact} />}
+        />
       </Routes>
+      <ContactForm
+        toggleContact={toggleContact}
+        setToggleContact={setToggleContact}
+      />
     </div>
   )
 }
