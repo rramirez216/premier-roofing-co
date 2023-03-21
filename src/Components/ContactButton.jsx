40114bc4fcbe7046d4c454c291652843239806c0
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-21 14:45:02 -0700

    changes text content of button element

--- src/Components/ContactButton.jsx
+++ src/Components/ContactButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ContactButton({ setToggleContact }) {
   return (
     <button className='btn' onClick={() => setToggleContact(true)}>
-      Get in Touch
+      Contact Us
     </button>
   )
 }
