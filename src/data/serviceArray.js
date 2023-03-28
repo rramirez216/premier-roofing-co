2781c69c9fb9db0632157605d56ed497041576b0
Ruben Ramirez <rramirez110911@gmail.com>
2023-03-27 20:09:56 -0700

    creats an array containing objects of services provided

---
+++ src/data/serviceArray.js
@@ -0,0 +1,58 @@
+let servicesProvidedArray = [
+  {
+    id: 1,
+    name: 'Roof Inspection',
+    description:
+      "A comprehensive assessment of the roof's condition to identify any damage or potential issues.",
+  },
+  {
+    id: 2,
+    name: 'Roof Repair',
+    description:
+      'Fixing leaks, cracks, and other damages to the roof to restore its integrity.',
+  },
+  {
+    id: 3,
+    name: 'Roof Replacement',
+    description:
+      'Full or partial replacement of a damaged or outdated roof with a new one.',
+  },
+  {
+    id: 4,
+    name: 'Roof Maintenance',
+    description:
+      'Regular cleaning and inspection of the roof to extend its lifespan and ensure its proper functioning.',
+  },
+  {
+    id: 5,
+    name: 'Roofing Material Selection',
+    description:
+      'Assistance with choosing the best roofing material for the building based on factors such as cost, durability, and appearance.',
+  },
+  {
+    id: 6,
+    name: 'Roof Installation',
+    description:
+      'Professional installation of the chosen roofing material, ensuring proper sealing, flashing, and ventilation.',
+  },
+  {
+    id: 7,
+    name: 'Roof Ventilation',
+    description:
+      'Installation of vents to regulate temperature and airflow in the attic, reducing the risk of mold and moisture damage.',
+  },
+  {
+    id: 8,
+    name: 'Skylight Installation',
+    description:
+      'Installation of skylights to increase natural light and improve energy efficiency.',
+  },
+  {
+    id: 9,
+    name: 'Gutter Cleaning and Repair',
+    description:
+      'Cleaning and fixing clogged or damaged gutters to ensure proper drainage and prevent water damage.',
+  },
+]
+
+export default servicesProvidedArray
