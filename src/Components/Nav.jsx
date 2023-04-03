552c2419d310921e2f386d36b978460b75d6c2a2
Ruben Ramirez <rramirez110911@gmail.com>
2023-04-03 15:09:09 -0700

    imports and uses useLocation hook

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/Logo.png'
 import { useScrollPosition } from '../hooks/useScrollPosition'
+import { useLocation } from 'react-router-dom'
 
 function Nav() {
   const scrollPosition = useScrollPosition()
   console.log(scrollPosition)
+  const { pathname } = useLocation()
 
   return (
     <nav
       className={`flex justify-center fixed top-0 left-0 right-0 z-10 ${
-        scrollPosition > 50 ? 'text-dark-blue bg-white shadow-lg' : 'text-white'
+        scrollPosition > 50 || pathname === '/services'
+          ? 'text-dark-blue bg-white shadow-lg'
+          : 'text-white'
       }  text-2xl`}
     >
       <div className='w-7/12 flex items-center gap-8'>
@@ -19,7 +23,9 @@ function Nav() {
         </Link>
         <div
           className={`border-t-2 border-b-0 ${
-            scrollPosition > 50 ? 'border-dark-blue' : 'border-white'
+            scrollPosition > 50 || pathname === '/services'
+              ? 'border-dark-blue'
+              : 'border-white'
           }  flex-auto`}
         ></div>
         <ul className='flex items-baseline gap-4 font-semibold'>
