ca35f375fb7e7bf372bb5d73e6d9ce8d37ede0d0
Ruben Ramirez <rramirez110911@gmail.com>
2023-04-25 14:42:15 -0700

    I'm back ! and added utility classes to change the padding on the left

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -4,7 +4,7 @@ import ContactButton from '../ContactButton'
 function Hero({ setToggleContact }) {
   return (
     <section className='w-full h-full bg-hero-pattern bg-scroll bg-cover pentagon overflow-hidden'>
-      <div className='w-full h-full flex flex-row items-center pl-64'>
+      <div className=' h-full flex flex-row items-center pl-16 md:pl-32 xl:pl-64'>
         <div className='w-96 flex flex-col items-start gap-12 -translate-y-20'>
           <h1 className='text-5xl'>
             <span className='text-light-orange'> BUILDING STRONG ROOFS</span>{' '}
