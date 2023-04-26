623ca1af34d1e7c99427782b869d31ba31fa0915
Ruben Ramirez <rramirez110911@gmail.com>
2023-04-26 14:57:59 -0700

    changes font size of h1 and changes gap between siblins

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -4,13 +4,13 @@ import ContactButton from '../ContactButton'
 function Hero({ setToggleContact }) {
   return (
     <section className='w-full h-full bg-hero-pattern bg-scroll bg-cover pentagon overflow-hidden'>
-      <div className=' h-full flex flex-row items-center pl-16 md:pl-32 xl:pl-64'>
-        <div className='w-96 flex flex-col items-start gap-12 -translate-y-20'>
-          <h1 className='text-5xl'>
+      <div className=' h-full flex flex-row items-center pl-16   md:pl-32 xl:pl-64'>
+        <div className='w-96 flex flex-col items-start gap-6 lg:gap-12 -translate-y-20'>
+          <h1 className='text-3xl xl:text-5xl'>
             <span className='text-light-orange'> BUILDING STRONG ROOFS</span>{' '}
             FOR STRONG HOMES
           </h1>
-          <h2 className='text-lg'>
+          <h2 className='text-base lg:text-lg'>
             So Cal's top-quality roofing service provider delivers long-term
             value and ensures the safety and protection of homes and businesses.
           </h2>
