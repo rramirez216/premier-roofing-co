c4f1e7cceebc5c81ea665e9ec305a705860fd162
Ruben Ramirez <rramirez110911@gmail.com>
2023-04-28 14:57:42 -0700

    padding and font size

--- src/Components/ContactButton.jsx
+++ src/Components/ContactButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ContactButton({ setToggleContact }) {
   return (
     <button
-      className='btn text-xl lg:text-2xl'
+      className='btn text-xl self-center lg:text-2xl'
       onClick={() => setToggleContact(true)}
     >
       Contact Us
