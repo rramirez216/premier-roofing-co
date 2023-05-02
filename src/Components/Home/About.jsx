dc3292602bf8035209ff79551bdf3ff148e505e5
Ruben Ramirez <rramirez110911@gmail.com>
2023-05-02 15:12:35 -0700

    adds responsive styles

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <section className='flex flex-col gap-8 md:flex-row text-dark-blue py-16 px-8 lg:px-0 lg:py-32'>
       <div className='lg:flex-1 flex flex-col items-center gap-8 lg:gap-0'>
-        <h2 className='text-2xl font-semibold'>WHO WE ARE</h2>
+        <h2 className='text-2xl font-semibold lg:mb-16'>WHO WE ARE</h2>
         <p className='lg:w-2/3 text-base'>
           Premier Roofing Co. is dedicated to providing exceptional roofing
           solutions that protect and enhance the value of our customers' homes
