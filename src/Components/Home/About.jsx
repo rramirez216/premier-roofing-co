3208581461509d3eae222a1da3e6bc7238709e9d
Ruben Ramirez <rramirez110911@gmail.com>
2023-05-05 14:57:21 -0700

    adds id attribute

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -4,7 +4,10 @@ import Two from '../../assets/workers2.jpg'
 
 function About() {
   return (
-    <section className='flex flex-col gap-8 md:flex-row text-dark-blue py-16 px-8 lg:px-0 lg:py-32'>
+    <section
+      className='flex flex-col gap-8 md:flex-row text-dark-blue py-16 px-8 lg:px-0 lg:py-32'
+      id='about'
+    >
       <div className='lg:flex-1 flex flex-col items-center gap-8 lg:gap-0'>
         <h2 className='text-2xl font-semibold lg:text-4xl lg:mb-16'>
           WHO WE ARE
