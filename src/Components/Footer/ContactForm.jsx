28cc39fb0ac67843021b6504d12aab03b2baad51
Ruben Ramirez <rramirez110911@gmail.com>
2023-05-05 14:58:37 -0700

    increases width of form element & increases height of text area input

--- src/Components/Footer/ContactForm.jsx
+++ src/Components/Footer/ContactForm.jsx
@@ -10,7 +10,7 @@ function ContactForm({ toggleContact, setToggleContact }) {
     >
       <form
         onSubmit={(e) => e.preventDefault()}
-        className=' flex flex-col gap-8 w-min bg-white text-dark-blue py-8 px-16 rounded-md relative'
+        className=' flex flex-col gap-8 w-min md:min-w-[576px] bg-white text-dark-blue py-8 px-16 rounded-md relative'
       >
         <div
           className='absolute top-0 right-0 text-dark-orange -translate-y-full cursor-pointer py-2'
@@ -23,21 +23,25 @@ function ContactForm({ toggleContact, setToggleContact }) {
           <label className='' htmlFor='full-name'>
             Full Name:
           </label>
-          <input type='text' className='custom-input' id='full-name' />
+          <input
+            type='text'
+            className='custom-input md:w-full'
+            id='full-name'
+          />
         </div>
         <div className='column-flex'>
           <label htmlFor='email'>Email:</label>
-          <input type='email' className='custom-input' id='email' />
+          <input type='email' className='custom-input md:w-full' id='email' />
         </div>
         <div className='column-flex'>
           <label className='' htmlFor='subject'>
             Subject:
           </label>
-          <input type='text' className='custom-input' id='subject' />
+          <input type='text' className='custom-input md:w-full' id='subject' />
         </div>
         <div className='column-flex'>
           <label htmlFor='message'>Message:</label>
-          <textarea id='message' className='bg-grey p-3 rounded-sm' />
+          <textarea id='message' className='bg-grey p-3 rounded-sm md:h-48' />
         </div>
         <button className='btn'>Send</button>
       </form>
