4258e891320bbba167fe0edd4d03687e9c7b6506
Ruben Ramirez <rramirez110911@gmail.com>
2023-05-05 15:00:11 -0700

    adds hover transitions to li

--- src/Pages/ServicesPage.jsx
+++ src/Pages/ServicesPage.jsx
@@ -25,7 +25,7 @@ function ServicesPage({
 
   return (
     <main className='text-dark-blue'>
-      <section className='flex justify-center pt-44 md:pt-48 pb-16 md:pb-32'>
+      <section className='flex justify-center pt-52 lg:pt-60 pb-16 lg:pb-24'>
         <div className='w-240 grid grid-cols-1 place-items-center lg:grid-cols-2 pl-16 pr-16 lg:pr-0'>
           <div className='min-h-[156px] md:max-w-[448px]'>
             <p className='text-sm'>Raising the Bar on Roofing</p>
@@ -41,7 +41,7 @@ function ServicesPage({
             <ul className='flex flex-col text-base lg:text-lg'>
               {servicesProvidedArray.map(({ id, name, description }) => (
                 <li
-                  className='py-2 px-8  border border-dark-blue cursor-pointer'
+                  className='py-2 px-8 border border-dark-blue cursor-pointer transition duration-200 hover:-translate-x-4 hover:border-light-orange'
                   key={id}
                   onClick={() =>
                     setServiceDisplayed({
