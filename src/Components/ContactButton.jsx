90e03cf4af0a9b61cdc47c62d9b1917702cee221
Ruben Ramirez <rramirez110911@gmail.com>
2023-05-05 14:54:13 -0700

    adds transition styles to button

--- src/Components/ContactButton.jsx
+++ src/Components/ContactButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ContactButton({ setToggleContact }) {
   return (
     <button
-      className='btn text-xl self-center lg:text-2xl'
+      className='btn text-xl self-center lg:text-2xl transition duration-200 hover:bg-darker-orange hover:scale-105'
       onClick={() => setToggleContact(true)}
     >
       Contact Us
