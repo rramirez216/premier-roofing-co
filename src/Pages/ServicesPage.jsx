c9a79223a30635a5a7a30b5b36c40a881bfe2a15
Ruben Ramirez <rramirez110911@gmail.com>
2023-06-03 18:21:04 -0700

    passes props

--- src/Pages/ServicesPage.jsx
+++ src/Pages/ServicesPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { styled } from 'styled-components'
 import Footer from '../Components/Footer/Footer'
 import hammer from '../assets/hammer.jpg'
 import servicesProvidedArray from '../data/serviceArray'
@@ -7,13 +8,19 @@ import { useScrollPosition } from '../hooks/useScrollPosition'
 function ServicesPage({
   serviceDisplayed: { name, description },
   setServiceDisplayed,
+  setToggleContact,
 }) {
-  const [percent, setPercent] = useState(50)
+  const [percent, setPercent] = useState(100)
   const scrollPosition = useScrollPosition()
-  useEffect(() => {
-    setPercent(percent - 10)
-  }, [scrollPosition])
+  // useEffect(() => {
+  //   if (scrollPosition > 0 && percent > 0) {
+  //     setPercent(percent - 10)
+  //   } else {
+  //     setPercent(percent + 10)
+  //   }
+  // }, [scrollPosition])
   // -translate-y-[${20}%]
+  // cant use dynamic values with arbitrary values
 
   return (
     <main className='text-dark-blue'>
@@ -29,9 +36,7 @@ function ServicesPage({
       <section className='flex justify-center py-16 lg:py-32 px-20 bg-off-white'>
         <div className='w-240 grid grid-cols-1 lg:grid-cols-2'>
           <div className='justify-self-center'>
-            <p className='font-bold text-2xl mb-8'>
-              Other services we offer: {`${percent}`}
-            </p>
+            <p className='font-bold text-2xl mb-8'>Other services we offer:</p>
             <ul className='flex flex-col text-base lg:text-lg'>
               {servicesProvidedArray.map(({ id, name, description }) => (
                 <li
@@ -49,19 +54,22 @@ function ServicesPage({
               ))}
             </ul>
           </div>
-          <div
-            className={
-              'w-[160px] justify-self-center self-end scale-[1.25]  hidden md:block'
-            }
+          <ImageWrapper
+            yaxis={`translateY(-${percent}%)`}
+            className={`w-[160px] justify-self-center self-end hidden lg:block`}
           >
             <img src={hammer} alt='hammer' />
-          </div>
+          </ImageWrapper>
         </div>
       </section>
-      <Footer />
+      <Footer setToggleContact={setToggleContact} />
     </main>
   )
 }
+const ImageWrapper = styled.div`
+  transform: ${(props) => props.yaxis} scale(1.25);
+  /* background-color: salmon; */
+`
 
 export default ServicesPage
 
